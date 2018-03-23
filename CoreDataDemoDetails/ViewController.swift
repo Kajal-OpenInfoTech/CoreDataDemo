dbd79b4180662dce1f8f0530225e59fa65ea9a26
Kajal <Kajal.openinfotech@gmail.com>
2018-03-23 19:08:04 +0530

    Initial Commit

---
+++ CoreDataDemoDetails/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreDataDemoDetails
+//
+//  Created by Mac 1 on 3/23/18.
+//  Copyright © 2018 OpenInfoTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
